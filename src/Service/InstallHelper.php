e59b391a519d7a78ac7b6df61243290c84e43b8e
dbo <dbo@makina-corpus.com>
2022-09-20 12:58:33 +0200

    Fix the create of medias folder.

--- src/Service/InstallHelper.php
+++ src/Service/InstallHelper.php
@@ -213,13 +213,10 @@ class InstallHelper {
    * @return $this
    */
   protected function prepareDirectoryForMedia(): ?self {
-    $realpath = $this->fileSystem->realpath(self::MEDIA_UPLOAD_DIRECTORY);
-    if (!\file_exists($realpath)) {
-      $this->fileSystem->prepareDirectory(
-        $realpath,
-        FileSystemInterface::CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS
-      );
-    }
+    $this->fileSystem->prepareDirectory(
+      $directory,
+      FileSystemInterface::CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS
+    );
 
     return $this;
   }
